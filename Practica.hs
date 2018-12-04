ce784c4da0e9ed8ee0b4285f47d796631e8d85ba
xabilahu <xabilahu99@hotmail.com>
2018-12-04 12:52:34 +0100

    Menu displaying of new features

--- Practica.hs
+++ Practica.hs
@@ -14,9 +14,11 @@ menu = do putStrLn "\n***Menú Principal***"
           let opcion = read opcionStr :: Integer
           case opcion of
               1 -> do tupla <- pedirPertenencia
-                      procesar_respuesta tupla
-              2 -> putStrLn "Funcionalidad a implementar en el entregable II."
-              3 -> putStrLn "Funcionalidad a implementar en el entregable II."
+                      procesar_respuesta_pert tupla
+              2 -> do det <- pedirClasificacion
+                      procesar_respuesta_clas det
+              3 -> do af <- pedirSimplificacion
+                      procesar_respuesta_simpl af
               4 -> putStrLn "Funcionalidad a implementar en el entregable III."
               5 -> return ()
               otherwise -> do putStrLn "Opción incorrecta, se volverá a lanzar el menú.\n"
@@ -33,30 +35,63 @@ submenu x y z = do putStrLn ("\n***Submenú de " ++ x ++ "***")
                    case opcion of
                        1 -> do putStr z
                                getLine
-                       2 -> return "No más palabras."
+                       2 -> return "Parar"
                        otherwise -> do putStrLn "Opción incorrecta, se volverá a lanzar el submenú."
                                        submenu x y z
 
-pedirPertenencia :: IO (Af,Palabra,Bool)
+pedirPertenencia :: IO (Af,Bool)
 pedirPertenencia = do putStr "\n\nIntroduzca el AF que define el lenguaje: "
                       afStr <- getLine
                       putStr "\nIntroduzca la palabra que quiera comprobar: "
                       palabraStr <- getLine
                       let af = read afStr :: Af
                       let palabra = read palabraStr :: Palabra
-                      return (af,palabra,(pertenencia af palabra))
+                      return (af,(pertenencia af palabra))
 
-procesar_respuesta :: (Af,Palabra,Bool) -> IO ()
-procesar_respuesta (af,palabra,pertenece) = do
+pedirClasificacion :: IO Bool
+pedirClasificacion = do putStr "\n\nIntroduzca el AF que quiera clasificar: "
+                        afStr <- getLine
+                        let af = read afStr :: Af
+                        return (determinista af)
+                    
+pedirSimplificacion :: IO Af
+pedirSimplificacion = do putStr "\n\nIntroduzca el AF que quiera simplificar: "
+                         afStr <- getLine
+                         let af = read afStr :: Af
+                         return (simplificacion af)
+
+procesar_respuesta_pert :: (Af,Bool) -> IO ()
+procesar_respuesta_pert (af,pertenece) = do
     if pertenece
         then putStrLn "\n\nPalabra perteneciente al lenguaje definido por el AF.\n\n"
         else putStrLn "\n\nPalabra no perteneciente al lenguaje definido por el AF.\n\n"
-    subOpt <- submenu ("pertenencia") ("Comprobar la pertenencia de otra palabra al lenguaje asociado al AF.") ("\nIntroduzca la sigiente palabra a comprobar: ")
-    if subOpt == "No más palabras."
+    subOpt <- submenu "pertenencia" "Comprobar la pertenencia de otra palabra al lenguaje asociado al AF." "\nIntroduzca la sigiente palabra a comprobar: "
+    if subOpt == "Parar"
         then menu
         else do let nuevaPalabra = read subOpt :: Palabra
-                let nuevoPertenece = pertenencia af nuevaPalabra
-                procesar_respuesta (af,nuevaPalabra,nuevoPertenece)
+                procesar_respuesta_pert (af,(pertenencia af nuevaPalabra))
+
+procesar_respuesta_clas :: Bool -> IO ()
+procesar_respuesta_clas det = do
+    if det
+        then putStrLn "\n\nEl AF introducido es determinista.\n\n"
+        else putStrLn "\n\nEl AF introducido no es determinista.\n\n"
+    subOpt <- submenu "clasificación" "Clasificar otro AF." "\nIntroduzca el siguiente AF a clasificar: "
+    if subOpt == "Parar"
+        then menu
+        else do let nuevoAF = read subOpt :: Af
+                procesar_respuesta_clas (determinista nuevoAF)
+
+procesar_respuesta_simpl :: Af -> IO ()
+procesar_respuesta_simpl af = do
+    let afStr = show af
+    let texto = "\n\nEl AF simplificado es " ++ afStr ++ ".\n\n"
+    putStrLn texto
+    subOpt <- submenu "simplificación" "Simplificar otro AF." "\nIntroduzca el siguiente AF a simplificar: "
+    if subOpt == "Parar"
+        then menu
+        else do let nuevoAF = read subOpt :: Af
+                procesar_respuesta_simpl (simplificacion nuevoAF)
 
 pertenencia :: Af -> Palabra -> Bool
 pertenencia (q,a,tau,sigma,y) word = pertenencia_aux (q,a,tau,sigma,y) word sigma []
@@ -96,35 +131,68 @@ pertenencia_aux (q,a,tau,sigma,y) word estActual porExaminar
 determinista :: Af -> Bool
 determinista (q,a,tau,sigma,y)
     | length tau == length [(qin,simb) | qin <- q, simb <- a] = True
-    --Generamos una lista con tantos componentes como transiciones debería haber desde cada estado si fuera AFD (tantas como simbolos contenga el alfabeto), si hay tantas transiciones en tau
-    --es AFD, sino es AFND.
+    --Generamos una lista con tantos componentes como transiciones debería haber desde cada estado si fuera AFD (número de estados * número de símbolos en el alfabeto).
+    --Si hay tantas transiciones en tau es AFD, sino es AFND.
     | otherwise = False
 
 alcanzables :: Af -> Estado -> Estados
-alcanzables (q,a,tau,sigma,y) estActual = elim_repetidos [qf | (qin,simb,qf) <- tau, qin == estActual]
---Se devuelve la lista con aquellos estados que aparecen como último elemento en las tuplas de tau en los que el primer elemento es estActual, eliminando aquellos estados que aparecen repetidos
+alcanzables (q,a,tau,sigma,y) estActual = alcanzables_aux (q,a,tau,sigma,y) estActual [] [] [estActual]
+--Se devuelve la lista de todos los estados que se pueden alcanzar desde es estado estActual.
+
+alcanzables_aux :: Af -> Estado -> Estados -> Estados -> Estados -> Estados
+alcanzables_aux (q,a,tau,sigma,y) estActual examinados resultado porExaminar
+--Se utiliza la lista examinados para ir almacenando los estados de los que ya se han obtenido los alcanzables y no ciclar.
+--Se utiliza la lista porExaminar para ir almacenando los estados de los que todavía no se han obtenido los alcanzables.
+    | length porExaminar == 0 = elim_repetidos resultado
+    --Cuando ya se han examinado todos los estados se devuelve la lista resultado sin repetidos.
+    | otherwise = alcanzables_aux (q,a,tau,sigma,y) (head(porExaminar)) ((head porExaminar):examinados) (alcanzablesDesde ++ resultado) ((tail porExaminar) ++ alcanzablesDesde)
+    --Si faltan estados por examinar, se toma el primer elemento de la lista porExaminar, se añade a examinados, se calculan todos los estados alcanzables desde el mismo y se añaden tanto a porExaminar como a resultado.
+    where alcanzablesDesde = elim_repetidos [qf | (qin,simb,qf) <- tau, qin == estActual, qf `notElem` examinados]
+    --alcanzablesDesde almacena la lista de estados que se alcanzan desde el estado actual y que no hayan sido examinados previamente (en caso de que ya hayan sido examinados ya estarán en la lista resultado).
+
 
 elim_repetidos :: (Eq t) => [t] -> [t]
 elim_repetidos [] = []
 elim_repetidos (x:s)
     | x `elem` s = elim_repetidos s
-    --Si x está repetido en el resto de la lista no lo añadimos a la lista resultado
+    --Si x está repetido en el resto de la lista no lo añadimos a la lista resultado.
     | otherwise = x : elim_repetidos s
-    --Si x no está repetido lo añadimos a la lista resultado
+    --Si x no está repetido lo añadimos a la lista resultado.
 
 aceptacion :: Af -> Estados
-aceptacion (q,a,tau,sigma,y) = elim_repetidos [qin | (qin,simb,qf) <- tau, qf `elem` y]
---Se devuelve la lista con aquellos estados que aparecen como primer elemento en las tuplas de tau en los que el último elemento pertenece a y (estados de aceptación)
+aceptacion (q,a,tau,sigma,y) = [x | x <- q, iterarEstAceptacion x (q,a,tau,sigma,y)] 
+--Se devuelve la lista de todos los estados desde los cuales se puede alcanzar un estado de aceptación.
+
+--([0,1,2,3,4],"abc",[(0,'a',1),(0,'a',2),(1,'b',3),(1,'c',4)],0,[4])
+
+iterarEstAceptacion :: Estado -> Af -> Bool
+--Se crea una función auxiliar cuya función es dado un estado x y el Af , devuelve true si existe al menos un estado de aceptación alcanzable desde x.
+--Esta función tiene como único objetivo calcular una única vez la lista de estados alcanzables desde x, de no implementar esta función, el coste sería mayor,
+--pues se estaría calculando una vez por cada elemento de y.
+iterarEstAceptacion x (q,a,tau,sigma,y) = length [z | z <- y, z `elem` alcanzablesDesde] > 0
+    where alcanzablesDesde = alcanzables (q,a,tau,sigma,y) x
 
 simplificacion :: Af -> Af
 simplificacion (q,a,tau,sigma,y)
     | determinista (q,a,tau,sigma,y) = (nuevoQDet,a,nuevoTauDet,sigma,nuevoYDet)
+    --En caso de ser un AFD, se eliminan aquellos estados que no son alcanzables desde el estado inicial y todas las transiciones correspondientes.
     | otherwise = (nuevoQNoDet,a,nuevoTauNoDet,sigma,nuevoYNoDet)
+    --En caso de ser un AFND, además de eliminar todos los estados que no son alcanzables desde el estado inicial y todas las transiciones correspondientes,
+    --se eliminan aquellos estados desde los cuales no es posible alcanzar un estado de aceptación.
     where
         nuevoQDet = elim_repetidos (sigma:(alcanzables (q,a,tau,sigma,y) sigma))
+        --Lista de estados resultante de eliminar todos aquellos estados que no son alcanzables desde el estado inicial.
         nuevoTauDet = [(qin,simb,qf) | (qin,simb,qf) <- tau, (qin `elem` nuevoQDet && qf `elem` nuevoQDet)]
+        --Lista de transiones resultante de eliminar aquellas que contienen algún estado no alcanzable desde el estado inicial.
         nuevoYDet = [qact | qact <- nuevoQDet, qact `elem` y]
+        --Lista de estados de aceptación resultante de eliminar aquellos estados no alcanzables desde el estado inicial.
         estadosAlcAceptacion = aceptacion (q,a,tau,sigma,y)
+        --Lista de estados desde los cuales es posible alcanzar un estado de aceptación
         nuevoQNoDet = elim_repetidos (sigma:[qact | qact <- nuevoQDet, qact `elem` estadosAlcAceptacion])
+        --Lista de estados resultante de eliminar además de los no alcanzables desde el estado inicial, los estados desde los cuales no es posible alcanzar uno de aceptación.
         nuevoTauNoDet = [(qin,simb,qf) | (qin,simb,qf) <- nuevoTauDet, (qin `elem` nuevoQNoDet && qf `elem` nuevoQNoDet)]
+        --Lista de transiciones resultante de eliminar además de la transiciones en las que aparece algún estado no alcanzable desde el estado inicial,
+        --las transiciones en las que aparece algún estado desde el cual no es posible alcanzar uno de aceptación.
         nuevoYNoDet = [qact | qact <- nuevoQNoDet, qact `elem` nuevoYDet]
+        --Lista de estados de aceptación resultante de eliminar además de quellos estados no alcanzables desde el estado inicial, los estados desde los cuales
+        --no es posible alcanzar uno de aceptación.
