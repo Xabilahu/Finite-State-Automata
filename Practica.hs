99d0cf97a7ab78724a9fe5721d00a8c3a3ad19e5
xabilahu <xabilahu99@hotmail.com>
2018-12-26 11:17:27 +0100

    Added menu support for DFA transformation operation

--- Practica.hs
+++ Practica.hs
@@ -19,12 +19,16 @@ menu = do putStrLn "\n***Menú Principal***"
                       procesar_respuesta_clas det
               3 -> do af <- pedirSimplificacion
                       procesar_respuesta_simpl af
-              4 -> putStrLn "Funcionalidad a implementar en el entregable III."
+              4 -> do af <- pedirTransformacion
+                      procesar_respuesta_trans af
               5 -> return ()
               otherwise -> do putStrLn "Opción incorrecta, se volverá a lanzar el menú.\n"
                               menu
 
 submenu :: String -> String -> String -> IO String
+--"x" es el título del menú, "y" es el mensaje de la primera opción del submenu y "z" es el mensaje para volver a introducir otro dato.
+--La finalidad de los tres parámetros de entrada es no tener código repetido para los distintos submenus de cada operación.
+--Esta función devuelve el dato intoducido por el usuario, en caso de no querer introducir nada más devuelve "Parar".
 submenu x y z = do putStrLn ("\n***Submenú de " ++ x ++ "***")
                    putStrLn "Elige una opción: "
                    putStrLn ("1. " ++ y)
@@ -40,6 +44,7 @@ submenu x y z = do putStrLn ("\n***Submenú de " ++ x ++ "***")
                                        submenu x y z
 
 pedirPertenencia :: IO (Af,Bool)
+--Devuelve como tupla tanto el Af como el resultado de la función pertenencia
 pedirPertenencia = do putStr "\n\nIntroduzca el AF que define el lenguaje: "
                       afStr <- getLine
                       putStr "\nIntroduzca la palabra que quiera comprobar: "
@@ -49,18 +54,28 @@ pedirPertenencia = do putStr "\n\nIntroduzca el AF que define el lenguaje: "
                       return (af,(pertenencia af palabra))
 
 pedirClasificacion :: IO Bool
+--Se devuelve el resultado de la función determinista
 pedirClasificacion = do putStr "\n\nIntroduzca el AF que quiera clasificar: "
                         afStr <- getLine
                         let af = read afStr :: Af
                         return (determinista af)
                     
 pedirSimplificacion :: IO Af
+--Se devuelve el resultado de la función simplificación
 pedirSimplificacion = do putStr "\n\nIntroduzca el AF que quiera simplificar: "
                          afStr <- getLine
                          let af = read afStr :: Af
                          return (simplificacion af)
 
+pedirTransformacion :: IO Af
+--Se devuelve el resultado de la función transformación
+pedirTransformacion = do putStr "\n\nIntroduzca el AF que quiera transformar en AFD: "
+                         afStr <- getLine
+                         let af = read afStr :: Af
+                         return (transformacion af)
+
 procesar_respuesta_pert :: (Af,Bool) -> IO ()
+--Muestra por pantalla el resultado y vuelve a pedir otra operación
 procesar_respuesta_pert (af,pertenece) = do
     if pertenece
         then putStrLn "\n\nPalabra perteneciente al lenguaje definido por el AF.\n\n"
@@ -72,6 +87,7 @@ procesar_respuesta_pert (af,pertenece) = do
                 procesar_respuesta_pert (af,(pertenencia af nuevaPalabra))
 
 procesar_respuesta_clas :: Bool -> IO ()
+--Muestra por pantalla el resultado y vuelve a pedir otra operación
 procesar_respuesta_clas det = do
     if det
         then putStrLn "\n\nEl AF introducido es determinista.\n\n"
@@ -83,6 +99,7 @@ procesar_respuesta_clas det = do
                 procesar_respuesta_clas (determinista nuevoAF)
 
 procesar_respuesta_simpl :: Af -> IO ()
+--Muestra por pantalla el resultado y vuelve a pedir otra operación
 procesar_respuesta_simpl af = do
     let afStr = show af
     let texto = "\n\nEl AF simplificado es " ++ afStr ++ ".\n\n"
@@ -92,6 +109,18 @@ procesar_respuesta_simpl af = do
         then menu
         else do let nuevoAF = read subOpt :: Af
                 procesar_respuesta_simpl (simplificacion nuevoAF)
+
+procesar_respuesta_trans :: Af -> IO ()
+--Muestra por pantalla el resultado y vuelve a pedir otra operación
+procesar_respuesta_trans af = do
+    let afStr = show af
+    let texto = "\n\nEl AF transformado es " ++ afStr ++ ".\n\n"
+    putStrLn texto
+    subOpt <- submenu "transformación" "Transformar otro AF." "\nIntroduzca el siguiente AF a transformar: "
+    if subOpt == "Parar"
+        then menu
+        else do let nuevoAF = read subOpt :: Af
+                procesar_respuesta_trans (transformacion nuevoAF)
 
 pertenencia :: Af -> Palabra -> Bool
 pertenencia (q,a,tau,sigma,y) word = pertenencia_aux (q,a,tau,sigma,y) word sigma []
@@ -116,17 +145,6 @@ pertenencia_aux (q,a,tau,sigma,y) word estActual porExaminar
     where
         lista = [qf | (qin,simb,qf) <- tau, (qin == estActual && simb == (head word))]
             --lista contiene los estados a los que se puede transicionar desde estActual con head word
-
---AFD prueba: lenguaje palabras que empiezan por 'a' y terminan en 'bc'
---([0,1,2,3,4],"abc",[(0,'a',1),(0,'b',4),(0,'c',4),(1,'a',1),(1,'b',2),(1,'c',1),(2,'a',1),(2,'b',2),(2,'c',3),(3,'a',1),(3,'b',2),(3,'c',1),(4,'a',4),(4,'b',4),(4,'c',4)],0,[3])
---AFD prueba: lenguaje palabras que contienen exactamente dos "a" o un número par de "b"
---([0,1,2,3,4,5,6,7],"abc",[(0,'a',1),(0,'b',4),(0,'c',0),(1,'a',2),(1,'b',5),(1,'c',1),(2,'a',3),(2,'b',6),(2,'c',2),(3,'a',3),(3,'b',7),(3,'c',3),(4,'a',5),(4,'b',1),(4,'c',4),(5,'a',6),(5,'b',1),(5,'c',5),(6,'a',7),(6,'b',2),(6,'c',6),(7,'a',7),(7,'b',3),(7,'c',7)],0,[0,1,2,3,6])
---AFND prueba: lenguaje palabras que contienen exactamente dos "a" o un número par de "b"
---([0,1,2,3,4,5,6,7,8],"abc",[(0,'a',8),(0,'a',1),(0,'b',4),(0,'c',0),(1,'a',2),(1,'b',5),(1,'c',1),(2,'a',3),(2,'b',6),(2,'c',2),(3,'a',3),(3,'b',7),(3,'c',3),(4,'a',5),(4,'b',1),(4,'c',4),(5,'a',6),(5,'b',1),(5,'c',5),(6,'a',7),(6,'b',2),(6,'c',6),(7,'a',7),(7,'b',3),(7,'c',7)],0,[0,1,2,3,6])
---AFND prueba: lenguaje de las palabras que contienen aa pero no c.
---([0,1,2],"abc",[(0,'a',0),(0,'b',0),(0,'a',1),(1,'a',2),(2,'a',2),(2,'b',2)],0,[2])
---AFD prueba (a impar) simplificacion resultado -> ([0,1],"ab",[(0,'a',1),(0,'b',0),(1,'a',0),(1,'b',1)],0,[1])
---([0,1,2],"ab",[(0,'a',1),(0,'b',0),(1,'a',2),(1,'b',1),(2,'a',1),(2,'b',2)],0,[1])
 
 determinista :: Af -> Bool
 determinista (q,a,tau,sigma,y)
@@ -215,3 +233,62 @@ simplificacion (q,a,tau,sigma,y)
         nuevoYNoDet = [qact | qact <- nuevoQNoDet, qact `elem` nuevoYDet]
         --Lista de estados de aceptación resultante de eliminar además de quellos estados no alcanzables desde el estado inicial, los estados desde los cuales
         --no es posible alcanzar uno de aceptación.
+
+automataCorrecto :: Af -> Bool
+automataCorrecto (q,a,tau,sigma,y) = and ((sigma `elem` q):[x `elem` q | x <- y] ++ [qin `elem` q && simb `elem` a && qf `elem` q | (qin,simb,qf) <- tau])
+-- Coste O(q*y + tau*(2q + a)) con respecto al número de transiciones, estados y símbolos del alfabeto.
+
+transformacion :: Af -> Af
+transformacion (q,a,tau,sigma,y)
+    | not (automataCorrecto (q,a,tau,sigma,y)) = error "Autómata incorrecto, no es posible ejecutar la transformación."
+    --Si el autómata es incorrecto se manda un mensaje de error
+    | determinista (q,a,tau,sigma,y) = (q,a,tau,sigma,y)
+    --Si el autómata es determinista directamente se devuelve el autómata
+    | otherwise = transAFND_AFD (q,a,tau,sigma,y) [[sigma]] [[sigma]] [] [] []
+    --Sino se devuelve el autómata transformado
+
+transAFND_AFD :: Af -> [Estados] -> [Estados] -> [Estados] -> [(Estados,Simbolo,Estados)] -> [Estados] -> Af
+transAFND_AFD (q,a,tau,sigma,y) porExaminar examinados nuevoQ nuevoTau nuevoY
+    | not (automataCorrecto (q,a,tau,sigma,y)) = error "Autómata incorrecto, no es posible ejecutar la transformación."
+    --Si el autómata es incorrecto se manda un mensaje de error
+    | length porExaminar == 0 && comprobarEstadoVacio nuevoTau = mapearAf ([[]] ++ nuevoQ, a, [([],'a',[]),([],'b',[]),([],'c',[])] ++ nuevoTau, sigma, [x | x <- nuevoY, x /= []]) (-1)
+    --Si desde algún estado se llega al estado vacío, se añade y se renombran los estados
+    | length porExaminar == 0 = mapearAf (nuevoQ,a,nuevoTau,sigma,[x | x <- nuevoY, x /= []]) 0
+    --Si no aparece el estado vacío directamente se renombran los estados
+    | otherwise = transAFND_AFD (q,a,tau,sigma,y) ((tail porExaminar) ++ alcanzablesSinExaminar) (alcanzablesSinExaminar ++ examinados) (nuevoQ ++ [(head porExaminar)]) (nuevoTau ++ (zip3 [(head porExaminar) | x <- a] a alcanzablesDesde)) (nuevoY ++ [(esFinal y (head porExaminar))])
+    --Si todavía quedan estados por examinar se generan los nuevos estados que se alcanzan desde el estado actual y se añaden a porEaminar y a examinados
+    --Además, el estado actual pasará a ser parte de la solución, por tanto se va construyendo la nueva lista de estados, la nueva lista de transiciones y
+    --de estados de aceptación
+    where alcanzablesDesde = [ms_rf(elim_repetidos [ qf | (qin, alpha, qf) <- tau, simb == alpha, qin `elem` (head porExaminar)]) | simb <- a ]
+          -- Primero se recorre el alfabeto, Por ejemplo:  (a , b , c) y por cada simbolo "x" se recorre la lista de transiciones
+          -- en la que compruebas que el símbolo de cada transición corresponde con "x" y que qin pertenece a la lista del estado actual
+          -- devolviendo el qf de las transiciones que cumplan las condiciones mencionadas.
+          --Los componentes de cada estado se ordenan y se eliminan los repetidos para que no aparezcan estados del tipo: [0,1] [1,1,0]
+          --pues ambos estados hacen referencia al mismo. 
+          alcanzablesSinExaminar = elim_repetidos [x | x <- alcanzablesDesde, x /= [], x `notElem` examinados]
+          --Se eliminan los estados repetidos para no añadirlos varias veces a porExaminar ni a examinados
+
+esFinal :: Estados -> Estados -> Estados
+esFinal y x
+    | any (`elem` y) x = x
+    --Si algún elemento de x perteneciente a los estados de aceptación se devuelve x
+    | otherwise = []
+    --Sino se devuelve lista vacía
+
+mapearAf :: ([Estados],Alfabeto,[(Estados,Simbolo,Estados)],Estado,[Estados]) -> Int -> Af
+mapearAf (nuevoQ,a,nuevoTau,sigma,nuevoY) idMenorEstado = (qFinal,a,tauFinal,sigma,yFinal)
+--idMenorEstado será -1 si hay estado vacío y 0 en caso contrario
+    where hashMap =  zip qFinal nuevoQ
+          --A cada estado se le asigna un identificador
+          qFinal = [toInteger x | x <- [idMenorEstado..((length nuevoQ) - 1 + idMenorEstado)]]
+          --Los id se convierten a Integer para que cuadre con la definición de tipos de Estado
+          tauInicial = [(x, simb, qf) | (qin,simb,qf) <- nuevoTau, (x,y) <- hashMap, qin == y]
+          --Se van modificando los estados por sus respectivos id en las transiciones
+          tauFinal = [(qin,simb,x) | (qin,simb,qf) <- tauInicial, (x,y) <- hashMap, qf == y]
+          --Se modifican los estados que quedaban por modificar de la lista de transiciones
+          yFinal = [x | q <- nuevoY, (x,y) <- hashMap, q == y]
+          --Se modifican los estados de aceptación por sus id correspondientes
+
+comprobarEstadoVacio :: [(Estados,Simbolo,Estados)] -> Bool
+comprobarEstadoVacio nuevoTau = or([qf == [] | ( qin, simb, qf) <- nuevoTau])
+--Se genera una lista de booleanos (True si qf es un estado vacío y False sino), con que algún elemento sea True se devuelve True
